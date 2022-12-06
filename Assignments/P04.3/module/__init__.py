f812982399fa2e3959466c34e992089b490e0050
CalebSneath <77464240+CalebSneath@users.noreply.github.com>
2022-12-06 13:16:12 -0600

    Create __init__.py
    Added __init__.py file.

---
+++ Assignments/P04.3/module/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ["timeconversion"]
+from module.timeconversion import convertTimeToSecondsSimple
+from module.timeconversion import convertTimeFromSecondsNoDate
+from module.timeconversion import convertTimeToSecondsNoDate
+from module.timeconversion import convertTimeToSeconds
+from module.timeconversion import convertTimeFromSeconds
+from module.timeconversion import convertDateToOtherDate
